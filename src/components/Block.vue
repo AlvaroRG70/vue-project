5736542c391a947ceba177a44409c5844e8df3b3
Alvaro Rodriguez <alvaro.rodriguez.garrido.al@iespoligonosur.org>
2024-01-16 12:30:54 +0000

    subido una parte

---
+++ src/components/Block.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="block" v-if="showBlock">
+        click me
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['delay'],
+        data(){
+            return{
+                showBlock: false
+            }
+        },
+        mounted() {
+            console.log('component mounted')
+            setTimeout(() => {
+                this.showBlock = true
+                console.log(this.delay)
+            }, this.delay)
+        },
+        updated(){
+            
+        }
+    }
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        background: lightblue;
+        color: black;
+        border-radius: 20px;
+        text-align: center;
+        padding: 100px 0;
+        margin: 50px auto;
+
+    }
+</style>
